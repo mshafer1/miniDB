c3ca08daa9dd6d4ddbde0bc3e5e9db78c20bf2a4
Matthew <2565361+mshafer1@users.noreply.github.com>
2018-10-20 15:57:51 -0500

    adding a testing strategy that will help keep track of whether the db is calling the storageStrategy correctly.

---
+++ DbXunitTests/TestStorageStrategy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiniDB;
+
+namespace DbXunitTests
+{
+    class NullWriterStorageStrategy : MiniDB.IStorageStrategy
+    {
+        public NullWriterStorageStrategy(): base()
+        {
+            this.WroteFlag = false;
+        }
+
+        public void cacheTransactions(ObservableCollection<DBTransaction> dBTransactions)
+        {
+           // NOOP
+        }
+
+        public void _cacheDB(DataBase db)
+        {
+            // NOOP
+            this.WroteFlag = true;
+        }
+
+        public ObservableCollection<DBTransaction> _getTransactionsCollection(string filename)
+        {
+            return new ObservableCollection<DBTransaction>();
+        }
+
+        public DataBase _loadDB(string filename)
+        {
+            return new DataBase("blah", 1, 1);
+        }
+
+        public void _migrate(float oldVersion, float newVersion)
+        {
+            // NOOP
+        }
+
+        public bool WroteFlag { get; private set; }
+
+        public void ClearWroteFlag()
+        {
+            this.WroteFlag = false;
+        }
+    }
+}

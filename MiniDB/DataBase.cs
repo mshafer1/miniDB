61d5f453544d1daf663060584ac3cff95a79c378
Matthew <2565361+mshafer1@users.noreply.github.com>
2019-05-28 19:03:06 -0500

    trying to get mono to handle dispose method correctly

--- MiniDB/DataBase.cs
+++ MiniDB/DataBase.cs
@@ -124,19 +124,19 @@ namespace MiniDB
             }
         }
 
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~DataBase() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
+        ~DataBase()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            this.Dispose(true);
+        }
 
         // This code added to correctly implement the disposable pattern.
         public void Dispose()
         {
             // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
+            this.Dispose(true);
             // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
+            GC.SuppressFinalize(this);
         }
         #endregion
 

2df770910bf08731c450955f28a86f1094968ee7
mshafer1 <mshafer1@harding.edu>
2019-07-05 17:17:17 -0500

    bump version number after push

--- metadata.sh
+++ metadata.sh
@@ -1,6 +1,6 @@
 id=MiniDataBase
 title=MiniDB
-version=0.0.2
+version=0.0.3
 authors=mshafer
 owners=mshafer
 description="Inspired by Biggy, a persistent Observable collection backed by JSON with undo/redo implemented"

e98cf04a3a8f9a4d1a4ecfe552ecf258415d9377
Matthew <2565361+mshafer1@users.noreply.github.com>
2019-07-05 14:55:18 -0500

    adding test project

---
+++ TestProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using MiniDB;
+
+namespace TestProject
+{
+    class Program
+    {
+        class DbObject : BaseDBObject, MiniDB.Interfaces.IDBObject
+        {
+            public string Name { get => this.Get(); set => this.Set(value); }
+        }
+
+        static void Main(string[] args)
+        {
+            var test = new MiniDB.DataBase("test.json", 1.0f, 1.0f);
+
+            var blah = new DbObject();
+            blah.Name = "test";
+            test.Add(blah);
+            blah.Name = "John Doe";
+            test.Undo();
+            test.Undo();
+        }
+    }
+}

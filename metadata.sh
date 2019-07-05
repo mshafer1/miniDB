4ec414f0820f039caa82abdd582d027d1524bc5a
mshafer1 <mshafer1@harding.edu>
2019-07-05 16:23:17 -0500

    get build working

---
+++ metadata.sh
@@ -0,0 +1,5 @@
+title=MiniDB
+version=0.0.1
+authors=mshafer1
+owners=mshafer1
+description="Inspired by Biggy, a persistent Observable collection backed by JSON with undo/redo implemented"
---
+++ setup_nuspec.sh
@@ -0,0 +1,9 @@
+source metadata.sh
+cp MiniDB/MiniDB.nuspec ./
+sed -i 's;\$version\$;'"$version"';' MiniDB.nuspec
+sed -i 's;\$title\$;'"$title"';' MiniDB.nuspec
+sed -i 's;\$id\$;'"$title"';' MiniDB.nuspec
+sed -i 's;\$authors\$;'"$authors"';' MiniDB.nuspec
+sed -i 's;\$owners\$;'"$owners"';' MiniDB.nuspec
+sed -i 's;\$description\$;'"$description"';' MiniDB.nuspec
+

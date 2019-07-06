c555858fa7e4a79bfb9e77d432a6aefd39ba8d97
Matthew <2565361+mshafer1@users.noreply.github.com>
2019-07-06 15:40:31 -0500

    moving output into _packages

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "release" ]; then
-nuget push $id.$version.nupkg -source nuget.org -apiKey $NUGET_API_KEY
+nuget push _packages/$id.$version.nupkg -source nuget.org -apiKey $NUGET_API_KEY
 fi

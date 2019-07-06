463aef5f60acb674692f5918f6a33821e362b81a
mshafer1 <mshafer1@harding.edu>
2019-07-06 15:59:17 -0500

    setting up test using package

--- testProject.sh
+++ testProject.sh
@@ -4,7 +4,7 @@ source metadata.sh
 
 FEED_PATH=`pwd`/_feed
 
-nuget init .\_packages $FEED_PATH
+nuget init ./_packages $FEED_PATH
 cd TestProject/TestProject
 
 cp -f _testProject.csproj TestProject.csproj
@@ -17,4 +17,6 @@ nuget restore TestProject.csproj -Source $FEED_PATH
 
 cd ..
 
-xbuild /p:Configuration=Release TestProject.sln
+msbuild /p:Configuration=Release TestProject.sln
+
+mono TestProject/bin/Release/TestProject.exe || exit -1
